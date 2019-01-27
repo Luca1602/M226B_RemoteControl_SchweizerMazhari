727e63d71932918142ab5c6f8b72cf8e87d03408
11tmazhari <tim.mazhari@sonova.com>
2019-01-27 15:48:01 +0100

    Added tests

---
+++ src/test/java/Testclass.java
@@ -0,0 +1,44 @@
+import RemoteControl.Plane;
+import RemoteControl.Ship;
+import RemoteControl.Truck;
+import RemoteControl.RemoteControl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.rmi.Remote;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Testclass {
+
+    //Creating "Fake" plane to test RemoteControl
+    @Mock
+    private Plane mockPlane;
+
+    @Test
+    public void testRemoteControl(){
+        RemoteControl remoteControl = new RemoteControl(mockPlane);
+        Assert.assertEquals(mockPlane, remoteControl.getMachine());
+    }
+
+    @Test
+    public void testFuelRemainingShip(){
+        Ship ship = new Ship();
+        Assert.assertEquals(90d, ship.fuelRemaining(), 0);
+    }
+
+    @Test
+    public void testFuelRemainingTruck(){
+        Truck truck = new Truck();
+        Assert.assertEquals(90d, truck.fuelRemaining(), 0d);
+    }
+
+    @Test
+    public void testFuelRemainingPlane(){
+        Plane plane = new Plane();
+        Assert.assertEquals(90d, plane.fuelRemaining(), 0d);
+    }
+}
